bd0b0ef7a80274240c6637eb2d3ea748cb0262da
luisjavierluna <javierlunaluis@gmail.com>
2023-08-14 11:39:30 -0600

    9. Limpiar país cuando el primer selector cambia

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PaisesService } from '../../services/paises.service';
 import { PaisSmall } from '../../interfaces/paises.interface';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -26,13 +27,15 @@ export class SelectorPageComponent implements OnInit {
     this.regiones = this.paisesService.regiones
 
     this.miFormulario.get('region')?.valueChanges
-      .subscribe(region => {
-        console.log(region);
-        this.paisesService.getPaisesPorRegion(region)
-          .subscribe(paises => {
-            console.log(paises)
-            this.paises = paises
-          })
+      .pipe(
+        tap( ( _ ) => {
+          this.miFormulario.get('pais')?.reset('')
+        }),
+        switchMap( region => this.paisesService.getPaisesPorRegion(region))
+      )
+      .subscribe(paises => {
+        this.paises = paises
+        
       })
   }
 
